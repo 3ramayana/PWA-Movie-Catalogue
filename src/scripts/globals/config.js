ffebeb54bcf16f2dcb1b785d8127f02f8c6a58da
Taufiq Ismail <67528742+3ramayana@users.noreply.github.com>
2023-05-12 05:53:57 +0900

    first commit

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,9 @@
+const CONFIG = {
+  KEY: '5e1def371adf3f5e96a88b0c083175ac',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+  CACHE_NAME: new Date().toISOString(),
+};
+
+export default CONFIG;
---
+++ src/scripts/utils/drawer-initiator.js
@@ -0,0 +1,23 @@
+const DrawerInitiator = {
+  init({ button, drawer, content }) {
+    button.addEventListener('click', (e) => {
+      this._toggleDrawer(e, drawer);
+    });
+
+    content.addEventListener('click', (e) => {
+      this._closeDrawer(e, drawer);
+    });
+  },
+
+  _toggleDrawer(event, drawer) {
+    event.stopPropagation();
+    drawer.classList.toggle('open');
+  },
+
+  _closeDrawer(event, drawer) {
+    event.stopPropagation();
+    drawer.classList.remove('open');
+  },
+};
+
+export default DrawerInitiator;
